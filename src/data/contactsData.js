e45ca6cadc651e45ecbe6f3490cb3dcd51b57392
Luis Rangel <luisrangelc@gmail.com>
2022-04-04 13:21:22 -0600

    feat: add new resume

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,6 +1,6 @@
 export const contactsData = {
-    email: 'janedoe.test@gmail.com',
-    phone: '+915588776600',
+    email: 'luisrangelc@gmail.com',
+    phone: '+50256917111',
     address: 'Menlo Park, California, United States - 673822 ',
 
     sheetAPI: ''
--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,9 @@
 import resume from '../assets/pdf/resume.pdf'
 
 export const headerData = {
-    name: 'Jane Doe',
+    name: 'Luis Rangel',
     title: "Web Developer",
     desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
-    image: 'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
+    image: 'https://lrangelc.github.io/images/profile.jpg',
     resumePdf: resume
 }
