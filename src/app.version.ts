e00231ebeafa331c102151250657298a6c9bb38b
Luis Rangel <luisrangelc@gmail.com>
2022-05-19 17:24:28 -0600

    feat: set app version file

--- src/app.version.ts
+++
@@ -1,2 +0,0 @@
-// This file was generated on Thu May 19 17:09:07 CST 2022
-export const appVersion = '0.1.1';
--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
-version_file=src/app.version.ts
+version_file=src/app.version.js
 > $version_file
 echo "// This file was generated on $(date)
 export const appVersion = '$1';" >> $version_file
